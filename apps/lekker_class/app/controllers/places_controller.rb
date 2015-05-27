7cecd5d2ce39878d6edd1bfc6b306ba2a2b57c73
bpenchas <bpenchas@gmail.com>
2015-05-27 14:32:45 +0200

    Got the index to work

--- apps/lekker_class/app/controllers/places_controller.rb
+++ apps/lekker_class/app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ActionController::Base
 	def index
+		@places = Place.all.order(:name)
 	end
 	def show
 	end
--- apps/lekker_class/db/seeds.rb
+++ apps/lekker_class/db/seeds.rb
@@ -6,5 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Place.delete_all
-Place.create! name: "Lion's Head", description: "Really tall and awesome."
-Place.create! name: "Table Mountain", description: "Even taller and more cool."
+Place.create! name: "Lion's Head", description: "Really tall and awesome.", address: '100 Kloof St'
+Place.create! name: "Table Mountain", description: "Even taller and more cool.", address: '100 Bree St'
+Place.create! name: "Knead", description: "Breakfast place.", address: '50 Kloof St'
