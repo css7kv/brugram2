400f0962da7c428234df71b522bcc46e43179421
bpenchas <bpenchas@gmail.com>
2015-05-28 17:34:42 +0200

    Finished fill in the blanks homework and bonus

--- apps/classroom/config/routes.rb
+++ apps/classroom/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   get 'welcome/index'
-  get 'welcome/students'
-  get 'welcome/students', to: 'welcome#students', as: 'student'
+  resources: students
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
